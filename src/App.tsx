4b5e31bc32687ce723ed199f7ef0e271796373e2
Marcin <marcinzmudka@gmail.com>
2020-10-10 11:53:52 +0200

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useRef } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+interface Coords {
+  latitude: number;
+  longitude: number;
+}
 function App() {
+  const ref = useRef<HTMLParagraphElement>(null);
+  const find = ({ coords }: { coords: Coords }) => {
+    const { latitude, longitude } = coords;
+    console.log("ASAS");
+    if (ref.current)
+      ref.current.innerText = `${latitude} ${longitude} | ${new Date()}`;
+  };
+  const options = {
+    enableHighAccuracy: true,
+    timeout: 100,
+    maximumAge: 0,
+  };
+  const error = () => {
+    if (ref.current) ref.current.innerText = "ERROR";
+  };
+  navigator.geolocation.watchPosition(find, error, options);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p ref={ref}>here should be your location</p>
       </header>
     </div>
   );
